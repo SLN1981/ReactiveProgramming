b71d2809b1423a3ed4e0f971004a975df876eea2
LN <narasimhan.ronaldinho007@gmail.com>
2022-06-25 18:47:05 +0530

    Movie Review Service

---
+++ movie-review-service/src/main/java/com/reactive/programming/exception/ReviewNotFoundException.java
@@ -0,0 +1,19 @@
+package com.reactive.programming.exception;
+
+public class ReviewNotFoundException extends RuntimeException {
+
+    private  String message;
+    private  Throwable ex;
+
+    public ReviewNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+        this.message = message;
+        this.ex = cause;
+    }
+
+    public ReviewNotFoundException(String message) {
+        super(message);
+        this.message = message;
+
+    }
+}
